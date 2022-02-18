f76b04f2dd3bf5efb4994f849f738ad3560ffdba
James R. Barlow <james@purplerock.ca>
2022-02-18 15:41:19 -0800

    Experiment with ccache

--- build-scripts/linux-build-wheel-deps.bash
+++ build-scripts/linux-build-wheel-deps.bash
@@ -1,21 +1,29 @@
 #!/bin/bash
 set -ex
 
+
+export CCACHE_DIR=$(pwd)/build-ccache
+mkdir -p $CCACHE_DIR
+
 if [ ! -f /usr/local/lib/libz.a ]; then
     pushd zlib
-    ./configure && make -j install
+    ./configure CC="ccache cc" CXX="ccache c++" && make -j install
     popd
 fi
 
 if [ ! -f  /usr/local/lib/libjpeg.a ]; then
     pushd jpeg
-    ./configure && make -j install
+    ./configure CC="ccache cc" CXX="ccache c++" && make -j install
     popd
 fi
 
 if [ ! -f /usr/local/lib/libqpdf.a ]; then
     pushd qpdf
-    ./configure --disable-oss-fuzz && make -j install-libs
+    if [[ $(uname -p) == 'aarch64' ]]; then
+        ./configure CC="ccache cc" CXX="ccache c++" --disable-oss-fuzz && make install
+    else
+        ./configure CC="ccache cc" CXX="ccache c++" --disable-oss-fuzz && make -j install
+    fi
     find /usr/local/lib -name 'libqpdf.so*' -type f -exec strip --strip-debug {} \+
     popd
 fi
